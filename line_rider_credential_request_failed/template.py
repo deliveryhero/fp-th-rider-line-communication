5ddf5b2c29fbc0ff6116e0678cec7c840fe168d9
Worawan Thanasanchiraphong <worawan.thanasanchiraphong@foodpanda.co.th>
2023-10-05 14:26:23 +0700

    DITH-1888 Update message template

--- line_rider_credential_request_failed/template.py
+++ line_rider_credential_request_failed/template.py
@@ -4,70 +4,74 @@ json_object = """
   "to": "{line_user_id}",
   "messages": [
     {
-      "type": "bubble",
-      "direction": "ltr",
-      "header": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "เกิดข้อผิดพลาด!",
-            "weight": "bold",
-            "size": "xxl",
-            "color": "#FF2B85",
-            "align": "center",
-            "contents": []
-          },
-          {
-            "type": "image",
-            "url": "https://pandariders.info/wp-content/uploads/2023/10/Untitled-10.png",
-            "margin": "none",
-            "size": "3xl"
+      "type": "flex",
+      "altText": "เกิดข้อผิดพลาด! ขออภัย ระบบไม่พบข้อมูล",
+      "contents": {
+        "type": "bubble",
+        "hero": {
+          "type": "image",
+          "url": "https://pandariders.info/wp-content/uploads/2023/10/Untitled-10.png",
+          "align": "center",
+          "gravity": "center",
+          "size": "xxl",
+          "aspectRatio": "3:3",
+          "aspectMode": "cover",
+          "action": {
+            "type": "uri",
+            "label": "Action",
+            "uri": "https://linecorp.com/"
           }
-        ]
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "ขออภัย ระบบไม่พบข้อมูล",
-            "weight": "bold",
-            "size": "xl",
-            "color": "#FF2B85",
-            "align": "center",
-            "gravity": "center",
-            "wrap": true,
-            "contents": []
-          },
-          {
-            "type": "text",
-            "text": "กรุณาติดต่อเจ้าหน้าที่ผ่านทางแชทบนหน้าเว็บไซต์แลกรางวัล",
-            "weight": "bold",
-            "color": "#000000",
-            "align": "center",
-            "wrap": true,
-            "contents": []
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "horizontal",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "คลิกเพื่อติดต่อฯ",
-              "uri": "https://ridershop.foodpanda.co.th/"
+        },
+        "body": {
+          "type": "box",
+          "layout": "vertical",
+          "spacing": "md",
+          "contents": [
+            {
+              "type": "text",
+              "text": "เกิดข้อผิดพลาด!",
+              "weight": "bold",
+              "size": "xxl",
+              "color": "#FF2B85",
+              "align": "center",
+              "gravity": "center",
+              "wrap": true,
+              "contents": []
             },
-            "color": "#FF2B85",
-            "gravity": "center"
-          }
-        ]
+            {
+              "type": "text",
+              "text": "ขออภัย ระบบไม่พบข้อมูล",
+              "weight": "bold",
+              "color": "#FF2B85",
+              "align": "center",
+              "contents": []
+            },
+            {
+              "type": "text",
+              "text": "กรุณาติดต่อเจ้าหน้าที่ผ่านทางแชทบนหน้าเว็บไซต์แลกรางวัล",
+              "align": "center",
+              "wrap": true,
+              "contents": []
+            }
+          ]
+        },
+        "footer": {
+          "type": "box",
+          "layout": "horizontal",
+          "flex": 1,
+          "contents": [
+            {
+              "type": "button",
+              "action": {
+                "type": "uri",
+                "label": "ติดต่อเจ้าหน้าที่",
+                "uri": "https://ridershop.foodpanda.co.th/"
+              },
+              "color": "#FF2B85",
+              "gravity": "center"
+            }
+          ]
+        }
       }
     }
   ]
